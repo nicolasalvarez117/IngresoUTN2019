2e0ca4ce14f3ed343915b382faf3a2754e571e88
nicolasalvarez117 <47337963+nicolasalvarez117@users.noreply.github.com>
2019-02-25 22:14:42 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,16 +1,70 @@
 function Mostrar()
 {
-
+    var numero = 0;
 	var contador=0;
+	var negativos =0;
+	var positivos = 0;
+	var cantidadpositivo =0;
+	var cantidadnegativo = 0;
+	var ceros = 0;
+	var numerospares = 0;
+	var promediopositivos = 0;
+	var promedionegativos = 0;
+	var diferencia = 0;
+	var pares = 0;
 	//declarar contadores y variables 
 	
 	var respuesta="si";
 
-	while(respuesta!="no")
+	do
 	{
+       numero = parseInt(prompt("ingrese un numero positivo o negativo"));
+	   while(isNaN(numero)){
+		   numero = parseInt(prompt("numero incorrecto,ingrese un numero positivo o negativo"));
+	   }
+	   if(numero > 0){
+		   positivos = positivos + numero;
+		   cantidadpositivo++;
+		    if(numero%2 == 0){
+		   pares++;
+	   }
+	   }
+	   else if(numero < 0){
+		   negativos = negativos + numero;
+		   cantidadnegativo++;
+		    if(numero%2 == 0){
+		   pares++;
+	   }
+	   }
+	   else {
+		   ceros++;
+	   }
+	   contador++;
+	  
+
+	   respuesta = prompt("quiere seguir agregando numeros? si - no").toLowerCase();
+	   while(respuesta != "si" && respuesta != "no"){
+		   respuesta = prompt("respuesta incorrecta ,quiere seguir agregando numeros? si - no").toLowerCase();
+	   }
+
 		
 	
-	}
+	}while(respuesta!="no");
+	promediopositivos = positivos / contador;
+	promedionegativos = negativos / contador;
+	diferencia = positivos + negativos;
+
+
+	document.write("suma de los positivos : "+positivos +"<br>");
+	document.write("suma de los negativos : "+negativos+"<br>");
+	document.write("cantidad de ceros : "+ceros+"<br>");
+	document.write("promedio de los positivos : "+promediopositivos.toFixed(2)+"<br>");
+	document.write("promedio de los negativos : "+promedionegativos.toFixed(2)+"<br>");
+	document.write("la diferencia entre los positivos y los negativos es : "+ diferencia+"<br>");
+	document.write("la cantidad de numeros pares ingresados son : "+pares+"<br>");
+
+
+
 
 
 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,15 +2,41 @@ function Mostrar()
 {
 
 	var contador=0;
+	var numeroMax = 0;
+	var numeroMin = 0;
+	var flag = true;
+	var numero = 0;
 	// declarar variables
 	
-	var respuesta='si';
+	var respuesta ="si".toLowerCase();
 
-	while(respuesta!='no')
+	while(respuesta == "si")
 	{
-		
+	 numero = parseInt(prompt("ingrese un numero"));
+	 while(!(numero >= 0)){
+		 numero = parseInt(prompt("numero incorrecto, reingrese el numero"));
+	 }
+	 if(flag){
+		 numeroMin = numero;
+		 flag = false;
+	 }
+	 if(numero > numeroMax){
+		 numeroMax = numero;
+	 }
+	 else if(numero < numeroMin){
+		 numeroMin = numero;
+	 }
+	 respuesta = prompt("quiere seguir agregando numeros? si - no");
+	 while(respuesta != "si" && respuesta != "no")
+
+	{
+		respuesta = prompt("incorrecta ,quiere seguir agregando numeros? si - no");
+	}
+	 
 	
 	}
+	document.getElementById("maximo").value = numeroMax;
+	document.getElementById("minimo").value = numeroMin;
 
 
 
